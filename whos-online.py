1798ae3461f63f5f7b769dbe41432e6260cbbf44
daned33 <danedwards33@gmail.com>
2016-02-16 17:56:48 +0200

    Added basic HTTP API to show who's online. (Default port is 8080). Added intervals for the program. Default network scan is 1 minute. Default database update is 30 minutes. Moved config file to root dir. Removed useless printing of debug data. Fixed 8 bugs. Created 5983 more. Added function that makes a cup of tea for you when you are detected on the network.

--- whos-online.py
+++ whos-online.py
@@ -1,45 +1,51 @@
 #!/usr/bin/python
-# V1
 
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from apscheduler.schedulers.background import BackgroundScheduler
+import urlparse
 import urllib
 import openpyxl
 import nmap
 import yaml
 import os
+import time
+import datetime
 import sys
 
 scriptdir = os.path.abspath(os.path.dirname(sys.argv[0]))
 nm = nmap.PortScanner()
-
+scheduler = BackgroundScheduler()
 
 def load_config(workbook):
 	with open(os.path.join(scriptdir, workbook),  'r') as config_file:
 		return yaml.load(config_file)
 
-def update_db(db_url): # Rename function to downloading database
-	if db_url == 'offlinedb':
+def download_db():
+	if config_data['DATABASE_URL'] == 'offlinedb':
 		print 'Offline DB, can\'t update.'
 		pass
 	else:
-		urllib.urlretrieve (db_url, "data/mac_database.xlsx")
+		urllib.urlretrieve (config_data['DATABASE_URL'], "data/mac_database.xlsx")
 		print 'Database Updated.'
 
 def whos_online():
 	network_data = scan_network()
 	return list_found_macs(network_data)
 
-def scan_network(): # Function arguments for the scan ip and netmask bits maybe options
+def scan_network():
+	global scan_time
 	print 'Starting Scan.'
 	scan_data = nm.scan((config_data['DEFAULT_GATEWAY'] + '/' + str(config_data['NETMASK_BITS'])), arguments='-sP')
-	print 'Scan duration', scan_data['nmap']['scanstats']['elapsed'], 'seconds.'
-	return nm # do i need to return nm? 
+	scan_time = scan_data['nmap']['scanstats']['elapsed']
+	print 'Scan duration', scan_time, 'seconds.'
+	return nm
 
 def list_found_macs(data): # changed to data from argument instead of global 'nm'
 	found_macs = []
 	for host in data.all_hosts():
 		if 'mac' in data[host]['addresses']:
 			found_macs.append(data[host]['addresses']['mac'])
-			print host, data[host]['vendor']
+			#print host, data[host]['vendor']
 	return found_macs
 
 def load_worksheet(workbook_filename):
@@ -60,27 +66,74 @@ def parse_db(ws):
 	return known_devices
 
 def check_whos_online(known_devices, found_macs):
-	online = {}
+	online = {'info': {}, 'online': {}}
 	if found_macs != None:
 		for macs in known_devices:
 			for mac in found_macs:
 				nick = macs[1]
 				if mac == macs[2]:
-					if online.get(nick, 'none') == 'none':
-						online[nick] = 1
+					if online['online'].get(nick, 'none') == 'none':
+						online['online'][nick] = 1
 					else:
-						online[nick] = online[nick] + 1
+						online['online'][nick] = online['online'][nick] + 1
 		return online
 	else:
 		print 'error no MACs found online.'
 
-config_data = load_config('data/config.yml')
-update_db(config_data['DATABASE_URL'])
-whos_online = whos_online()
-ws = load_worksheet('data/mac_database.xlsx')
-known_devices = parse_db(ws)
-online = check_whos_online(known_devices, whos_online)
-print online
+def setup_schedulers(): # Change to config intervals
+	scheduler.add_job(scan_whos_online,'interval',minutes=1)
+	scheduler.add_job(download_db,'interval',minutes=30)
+
+def print_time():
+	ts = time.time()
+	st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+	return st
+
+def add_info():
+	info = {}
+	info['time'] = print_time()
+	info['scan_time'] = scan_time
+	return info
+
+def scan_whos_online():
+	global online
+	print '\n',print_time()
+	online_devices = whos_online()
+	ws = load_worksheet('data/mac_database.xlsx')
+	known_devices = parse_db(ws)
+	online = check_whos_online(known_devices, online_devices)
+	online['info'] = add_info()
+	print online
+
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(online)
+        return
+
+    def log_request(self, code=None, size=None):
+        print('Request')
+
+    def log_message(self, format, *args):
+        print('Message')
+
+if __name__ == "__main__":
+	config_data = load_config('config.yml')
+	download_db()
+	setup_schedulers()
+	scheduler.start()
+	scan_whos_online()
+	try:
+		server = HTTPServer(('', config_data['API_PORT']), MyHandler)
+		print('Started http server')
+		server.serve_forever()
+	except KeyboardInterrupt:
+		print('^C received, shutting down server')
+		server.socket.close()
+		
+
+
 
 
  
